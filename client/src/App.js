1ab8a4756f3b6ccd51b055caad481fa1b597407d
Nils Rohe <nils.rohe@sosafe.de>
2019-09-10 21:47:12 +0200

    added first navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+import SiteNavbar from './components/SiteNavbar';
+import LandingPage from './components/LandingPage';
+import SignUpForm from './components/SignUpForm';
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <h3>MyWisdom App</h3>
-        <p>App will be here.</p>
+        <SiteNavbar />
+        <main>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/signup" component={SignUpForm} />
+        </main>
+        
       </div>
     )
   }
---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class SignUpForm extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the sign up form</p>
+        <p>This is the sign up form</p>
+        <p>This is the sign up form</p>
+        <p>This is the sign up form</p>
+        <p>This is the sign up form</p>
+      </div>
+    )
+  }
+}
